23e00749b237e7dde90b536ecdfe5a337750413f
sekoudosso82 <sekou.dosso82@gmail.com>
2020-08-11 17:39:07 -0400

    Updates

---
+++ precache-manifest.d4201268cbcfd8e4b2a67e97ebf0c7fe.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b52b013df29badb6d6313b1504ebd0bd",
+    "url": "/corona-market-front-end/index.html"
+  },
+  {
+    "revision": "3cd998fd886e319807d0",
+    "url": "/corona-market-front-end/static/css/main.f71af11c.chunk.css"
+  },
+  {
+    "revision": "62c025cd2807e761fe91",
+    "url": "/corona-market-front-end/static/js/2.e6bad72a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/corona-market-front-end/static/js/2.e6bad72a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3cd998fd886e319807d0",
+    "url": "/corona-market-front-end/static/js/main.29c80af6.chunk.js"
+  },
+  {
+    "revision": "2a046645b31722acebd8",
+    "url": "/corona-market-front-end/static/js/runtime-main.2fc5aa13.js"
+  },
+  {
+    "revision": "7479a9f91cc9924d6ff3387f3cb7a020",
+    "url": "/corona-market-front-end/static/media/bp.7479a9f9.jpg"
+  },
+  {
+    "revision": "cd8dd53e577a484a9d70ad992f6c7684",
+    "url": "/corona-market-front-end/static/media/bp3.cd8dd53e.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/corona-market-front-end/static/media/logo.5d5d9eef.svg"
+  }
+]);
